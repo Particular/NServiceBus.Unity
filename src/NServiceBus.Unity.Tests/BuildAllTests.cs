8a4514b9ffd0cbb321482268928caa9f01d18a52
Andreas Öhlund <andreas.ohlund@particular.net>
2014-09-02 20:43:45 +0200

    Adding unit tests

---
+++ src/NServiceBus.Unity.Tests/BuildAllTests.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.Unity.Tests
+{
+    using NUnit.Framework;
+    using System.Linq;
+
+    [TestFixture]
+    public class BuildAllTests
+    {
+        [Test]
+        public void ShouldResolveAllInterfaces()
+        {
+            var builder = new UnityObjectBuilder();
+
+
+            builder.Configure(typeof(First), DependencyLifecycle.InstancePerCall);
+
+            builder.Configure(typeof(Second), DependencyLifecycle.InstancePerCall);
+
+            var result = builder.BuildAll(typeof(ISomeInterface));
+
+            Assert.AreEqual(2, result.Count());
+        }
+
+        class First:ISomeInterface
+        {
+
+        }
+
+        class Second : ISomeInterface
+        {
+
+        }
+
+        interface ISomeInterface
+        {
+
+        }
+    }
+}
