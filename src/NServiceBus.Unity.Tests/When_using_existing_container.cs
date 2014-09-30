a12d0f23f4491142f4112ae2be98761cd2f2073b
SzymonPobiega <szymon.pobiega@gmail.com>
2014-09-30 08:49:40 +0200

    Added a failing test for #9

---
+++ src/NServiceBus.Unity.Tests/When_using_existing_container.cs
@@ -0,0 +1,32 @@
+﻿namespace NServiceBus.Unity.Tests
+{
+    using Microsoft.Practices.Unity;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_using_existing_container
+    {
+        [Test]
+        public void Components_registered_in_plain_container_are_resolvable_via_builder()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<SomeClass>();
+
+            var builder = new UnityObjectBuilder();
+
+            var result = builder.Build(typeof(ISomeInterface));
+
+            Assert.IsInstanceOf<SomeClass>(result);
+        }
+
+        class SomeClass : ISomeInterface
+        {
+
+        }
+
+        interface ISomeInterface
+        {
+
+        }
+    }
+}
