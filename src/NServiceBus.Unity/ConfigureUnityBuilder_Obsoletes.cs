a1cd426c2d5a575621c71bf7674b468cacc6a948
Simon Cropp <simon.cropp@gmail.com>
2015-09-11 09:55:26 +1000

    bump refs

--- src/NServiceBus.Unity/ConfigureUnityBuilder_Obsoletes.cs
+++ src/NServiceBus.Unity/ConfigureUnityBuilder_Obsoletes.cs
@@ -1,29 +1,31 @@
 ﻿#pragma warning disable 1591
 // ReSharper disable UnusedParameter.Global
+
 namespace NServiceBus
 {
     using System;
     using Microsoft.Practices.Unity;
 
     [ObsoleteEx(
-        Message = "Replace with Use `configuration.UseContainer<UnityBuilder>()`, where `configuration` is an instance of type `BusConfiguration`.",
         TreatAsErrorFromVersion = "6.0",
-        RemoveInVersion = "7.0")]
+        RemoveInVersion = "7.0",
+        ReplacementTypeOrMember = "BusConfiguration.UseContainer<UnityBuilder>()"
+        )]
     public static class ConfigureUnityBuilder
     {
         [ObsoleteEx(
-            Message = "Replace with Use `configuration.UseContainer<UnityBuilder>()`, where `configuration` is an instance of type `BusConfiguration`.",
             TreatAsErrorFromVersion = "6.0",
-            RemoveInVersion = "7.0")]
+            RemoveInVersion = "7.0",
+            ReplacementTypeOrMember = "BusConfiguration.UseContainer<UnityBuilder>()")]
         public static Configure UnityBuilder(this Configure config)
         {
             throw new NotImplementedException();
         }
 
         [ObsoleteEx(
-            Message = "Use `configuration.UseContainer<UnityBuilder>(b => b.UseExistingContainer(applicationContext))`, where `configuration` is an instance of type `BusConfiguration`.",
             TreatAsErrorFromVersion = "6.0",
-            RemoveInVersion = "7.0")]
+            RemoveInVersion = "7.0",
+            ReplacementTypeOrMember = "BusConfiguration.UseContainer<UnityBuilder>(b => b.UseExistingContainer(applicationContext))")]
         public static Configure UnityBuilder(this Configure config, IUnityContainer container)
         {
             throw new NotImplementedException();
