6238efcf22e9c862ad8c44f1f00a3c619730f64c
Daniel Marbach <daniel.marbach@particular.net>
2016-09-23 16:47:24 +0200

    Added missing SetUpFixture (test were running with Autofac)

---
+++ src/NServiceBus.Unity.Tests/SetUpFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace NServiceBus.Unity.Tests
+{
+    using NServiceBus.ContainerTests;
+    using NUnit.Framework;
+
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        public SetUpFixture()
+        {
+            TestContainerBuilder.ConstructBuilder = () => new UnityObjectBuilder();
+        }
+    }
+}
