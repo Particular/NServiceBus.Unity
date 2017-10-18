6b1d76b47a02f2fe8c4ce228041d50c4c41d093a
Bob Langley <bob.langley@particular.net>
2017-10-18 16:58:28 -0700

    Adds diagnostics reporting if customer is supplying an existing container

---
+++ src/NServiceBus.Unity/UnityDiagnostics.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Features
+{
+    /// <summary>
+    /// Adds Diagnostics information
+    /// </summary>
+    public class UnityDiagnostics : Feature
+    {
+        /// <summary>
+        /// Constructor for diagnostics feature
+        /// </summary>
+        public UnityDiagnostics()
+        {
+            EnableByDefault();
+        }
+
+        /// <summary>
+        /// Sets up diagnostics
+        /// </summary>
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Settings.AddStartupDiagnosticsSection("NServiceBus.Unity", new
+            {
+                UsingExistingContainer = context.Settings.HasSetting<UnityBuilder.ContainerHolder>()
+            });
+        }
+    }
+}
