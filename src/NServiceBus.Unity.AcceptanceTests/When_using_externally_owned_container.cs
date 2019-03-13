e4679d425b34544165a0eae0cffa816f1a4d7662
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-03-13 16:08:15 +0100

    adjust disposal tests to no longer need a container decorator

--- src/NServiceBus.Unity.AcceptanceTests/When_using_externally_owned_container.cs
+++ src/NServiceBus.Unity.AcceptanceTests/When_using_externally_owned_container.cs
@@ -1,7 +1,9 @@
 ﻿namespace ObjectBuilder.Unity.AcceptanceTests
 {
+    using System;
     using System.Threading.Tasks;
     using global::Unity;
+    using global::Unity.Extension;
     using NServiceBus;
     using NServiceBus.AcceptanceTesting;
     using NServiceBus.AcceptanceTests;
@@ -10,40 +12,41 @@
 
     public class When_using_externally_owned_container : NServiceBusAcceptanceTest
     {
-        //[Test]
-        //public async Task Should_shutdown_properly()
-        //{
-        //    var context = await Scenario.Define<Context>()
-        //        .WithEndpoint<Endpoint>()
-        //        .Done(c => c.EndpointsStarted)
-        //        .Run();
-
-        //    Assert.IsFalse(context.Decorator.Disposed);
-        //    Assert.DoesNotThrow(() => context.Container.Dispose());
-        //}
-
-        class Context : ScenarioContext
+        [Test]
+        public async Task Should_shutdown_properly()
         {
-            public IUnityContainer Container { get; set; }
-            public ContainerDecorator Decorator { get; set; }
+            var container = new UnityContainer();
+            var disposableExtension = new DisposableExtension();
+
+            await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<Endpoint>(e => e.CustomConfig(config => config.UseContainer<UnityBuilder>(c => c.UseExistingContainer(container))))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsFalse(disposableExtension.Disposed);
+            Assert.DoesNotThrow(() => container.Dispose());
+
+        }
+
+        // Extensions implementing IDisposable are disposed as part of disposing the UnityContainer
+        // so we can use this property to assert dispose calls.
+        class DisposableExtension : IUnityContainerExtensionConfigurator, IDisposable
+        {
+            public IUnityContainer Container { get; }
+
+            public bool Disposed { get; private set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
         }
 
         class Endpoint : EndpointConfigurationBuilder
         {
             public Endpoint()
             {
-                EndpointSetup<DefaultServer>((config, desc) =>
-                {
-                    config.SendFailedMessagesTo("error");
-                    var container = new UnityContainer();
-                    var decorator = new ContainerDecorator(container);
-
-                    config.UseContainer<UnityBuilder>(c => c.UseExistingContainer(decorator));
-
-                    var context = (Context)desc.ScenarioContext;
-                    context.Container = container;
-                    context.Decorator = decorator;
-                });
+                EndpointSetup<DefaultServer>();
             }
         }
     }
--- src/NServiceBus.Unity.Tests/DisposalTests.cs
+++ src/NServiceBus.Unity.Tests/DisposalTests.cs
@@ -1,90 +1,52 @@
 ﻿namespace NServiceBus.Unity.Tests
 {
     using System;
-    using System.Collections.Generic;
     using global::Unity;
     using global::Unity.Extension;
-    using global::Unity.Injection;
-    using global::Unity.Lifetime;
-    using global::Unity.Registration;
-    using global::Unity.Resolution;
     using NUnit.Framework;
     using Unity;
 
     [TestFixture]
     public class DisposalTests
     {
-        //[Test]
-        //public void Owned_container_should_be_disposed()
-        //{
-        //    var fakeContainer = new FakeContainer();
-
-        //    var container = new UnityObjectBuilder(fakeContainer, true);
-        //    container.Dispose();
-
-        //    Assert.True(fakeContainer.Disposed);
-        //}
-
-        //[Test]
-        //public void Externally_owned_container_should_not_be_disposed()
-        //{
-        //    var fakeContainer = new FakeContainer();
-
-        //    var container = new UnityObjectBuilder(fakeContainer, false);
-        //    container.Dispose();
-
-        //    Assert.False(fakeContainer.Disposed);
-        //}
-
-        class FakeContainer : IUnityContainer
+        [Test]
+        public void Owned_container_should_be_disposed()
         {
+            var ownedContainer = new UnityContainer();
+            var disposableExtension = new DisposableExtension();
+            ownedContainer.AddExtension(disposableExtension);
+
+            var container = new UnityObjectBuilder(ownedContainer, true);
+            container.Dispose();
+
+            Assert.True(disposableExtension.Disposed);
+        }
+
+        [Test]
+        public void Externally_owned_container_should_not_be_disposed()
+        {
+            var externalContainer = new UnityContainer();
+            var disposableExtension = new DisposableExtension();
+            externalContainer.AddExtension(disposableExtension);
+
+            var container = new UnityObjectBuilder(externalContainer, false);
+            container.Dispose();
+
+            Assert.False(disposableExtension.Disposed);
+        }
+
+        // Extensions implementing IDisposable are disposed as part of disposing the UnityContainer
+        // so we can use this property to assert dispose calls.
+        class DisposableExtension : IUnityContainerExtensionConfigurator, IDisposable
+        {
+            public IUnityContainer Container { get; }
+
             public bool Disposed { get; private set; }
 
             public void Dispose()
             {
                 Disposed = true;
             }
-
-            public object Resolve(Type t, string name, params ResolverOverride[] resolverOverrides)
-            {
-                return null;
-            }
-
-            public object BuildUp(Type t, object existing, string name, params ResolverOverride[] resolverOverrides)
-            {
-                return null;
-            }
-
-            public IUnityContainer CreateChildContainer()
-            {
-                return null;
-            }
-
-            public IUnityContainer RegisterType(Type typeFrom, Type typeTo, string name, ITypeLifetimeManager lifetimeManager, params InjectionMember[] injectionMembers)
-            {
-                return null;
-            }
-
-            public IUnityContainer RegisterInstance(Type type, string name, object instance, IInstanceLifetimeManager lifetimeManager)
-            {
-                return null;
-            }
-
-            public IUnityContainer RegisterFactory(Type type, string name, Func<IUnityContainer, Type, string, object> factory, IFactoryLifetimeManager lifetimeManager)
-            {
-                return null;
-            }
-
-            public bool IsRegistered(Type type, string name)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IUnityContainer Parent { get; }
-
-            IEnumerable<IContainerRegistration> registrations = new List<IContainerRegistration>();
-
-            IEnumerable<IContainerRegistration> IUnityContainer.Registrations => registrations;
         }
     }
 }
